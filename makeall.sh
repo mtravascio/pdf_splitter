a574d859b64f79350ba38b2018182ecda948e6e4
mtravascio <m.travascio@gmail.com>
2025-05-22 11:33:56 +0200

    modificato il makefile per fare il build anche del flatpak

--- makeall.sh
+++ makeall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Cleaning up previous builds"
+flutter clean
+
 echo "Building for Linux"
 flutter build linux --release
 
@@ -23,19 +26,21 @@ fastforge package --platform linux --targets zip
 
 cd ./linux/pdf_splitter_app
 echo "Building for Linux flatpak"
+rm -rf ~/.flatpak-builder/
 mkdir -p bundle
 cp -r ../../build/linux/x64/release/bundle/* bundle/
+cp ../../assets/64.png bundle/net.regeomaria.pdf_splitter.png
+cp ../../assets/pdf_splitter.desktop bundle/net.regeomaria.pdf_splitter.desktop
 
 #flatpak-builder --force-clean build-dir flatpak/manifest.yaml
 
 #flatpak-builder --run build-dir flatpak/manifest.yaml pdf_splitter #per eseguirlo!
 
-
 flatpak-builder --repo=repo --force-clean build-dir flatpak/manifest.yaml #Crea la dir repo!
 
 flatpak build-bundle repo pdf_splitter.flatpak net.regeomaria.pdf_splitter #Crea il file pdf_splitter.flatpak! nella dir corrente
 
-flatpak install --reinstall --user pdf_splitter.flatpak #Installa il pacchetto flatpak appena creato
+flatpak install --reinstall pdf_splitter.flatpak #Installa il pacchetto flatpak appena creato
 
 flatpak run net.regeomaria.pdf_splitter #Esegue il pacchetto flatpak appena creato
 
